fa4b81d065c00ad7f1bc4224b30d69c682c1f63c
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2013-04-28 13:31:12 +0100

    Added onerror handler

--- public/js/failbail.js
+++ public/js/failbail.js
@@ -1,3 +1,23 @@
 window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
-  alert('errorMsg: ' + errorMsg);
+  var msg = {};
+  msg.errorMsg = errorMsg;
+  msg.url = url;
+  msg.lineNumber = lineNumber;
+
+  console.log(msg);
+
+  /*
+  $.ajax({
+    url: 'http://localhost:5000/',
+    data: msg
+  }).done(function ( data ) {
+    if(console && console.log ) {
+      console.log(data);
+      alert('error: ' + msg);
+    }
+  }).fail(function () {
+    alert("error");
+  });
+  */
+
 };
